55e6b553d06c1df59d7349316a914f1fcd978dfc
Watley <watlinton@hotmail.com>
2017-10-30 23:12:40 +0200

    added files to repo

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var scrollLink = $('.scroll');
+	
+	//smooth scrolling:
+	scrollLink.click(function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop:$(this.hash).offset().top - 40 //how far away from the top is this section
+		},2500);										//then bring the scroll bar to the current position
+	})	
+		
+})
